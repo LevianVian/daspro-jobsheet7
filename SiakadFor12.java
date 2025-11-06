81430d747b77ed5ab493f8b9667ea0ea8881ccc6
LevianVian <artalevi4@gmail.com>
2025-11-06 10:55:55 +0700

    Refactor SiakadFor12 class for improved readability and consistency in output messages

--- SiakadFor12.java
+++ SiakadFor12.java
@@ -2,27 +2,36 @@ import java.util.Scanner;
 
 public class SiakadFor12{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
+                Scanner sc = new Scanner(System.in);
 
-        double grade, highest, lowest; 
-        highest = 0;   
-        lowest = 100;  
+        double grade, highest, lowest;
+        int pass = 0, fail = 0;
+
+        highest = 0;
+        lowest = 100;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Enter the score for student " + i + " : " );
+            System.out.print("Enter the grade of the " + i + " student: ");
             grade = sc.nextDouble();
 
             if (grade > highest) {
                 highest = grade;
             }
-
             if (grade < lowest) {
                 lowest = grade;
+            }
+
+            if (grade >= 60) {
+                pass++;
+            } else {
+                fail++;
             }
         }
 
         System.out.println("The highest grade: " + highest);
         System.out.println("The lowest grade: " + lowest);
+        System.out.println("Number of students passed: " + pass);
+        System.out.println("Number of students failed: " + fail);
 
         sc.close();
     }
