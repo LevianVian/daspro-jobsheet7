2ea0ab346e54b4d501c5aab46a8f4b7e44f62e79
LevianVian <artalevi4@gmail.com>
2025-11-06 11:44:51 +0700

    Create KafeDoWhile12 class to handle customer orders and calculate total prices

---
+++ KafeDoWhile12.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class KafeDoWhile12{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int coffee, tea, bread;
+        int coffeePrice = 12000;
+        int teaPrice = 7000;
+        int breadPrice = 20000;
+        int totalPrice;
+        String customerName;
+        
+        do {
+            System.out.print("Enter customer name (type 'cancel' to exit): ");
+            customerName = sc.nextLine();
+            
+            if (customerName.equalsIgnoreCase("cancel")) {
+                System.out.println("Transaction canceled.");
+                break;
+            }
+            
+            System.out.print("Number of coffees: ");
+            coffee = sc.nextInt();
+            
+            System.out.print("Number of teas: ");
+            tea = sc.nextInt();
+            
+            System.out.print("Number of breads: ");
+            bread = sc.nextInt();
+            
+            totalPrice = (coffee * coffeePrice) + (tea * teaPrice) + (bread * breadPrice);
+            
+            System.out.println("Total to be paid by " + customerName + ": Rp " + totalPrice);
+            
+            sc.nextLine();            
+        } while (true);
+        
+        System.out.println("All transactions completed.");
+        sc.close();
+    }
+}
